8e9051c0f0128f5f42f71af5064df6a67303703c
prithvi-jpg <mv.prithvi@gmail.com>
2021-09-03 19:53:18 +0530

    Deployed to firebase, V0.5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBv9WX81IUH8OQoJMiPd_U_pZsfAuHxvtE',
+    authDomain: 'worktaskfire.firebaseapp.com',
+    databaseURL: 'https://worktaskfire.firebaseio.com',
+    projectId: 'worktaskfire',
+    storageBucket: 'worktaskfire.appspot.com',
+    messagingSenderId: '233126890615',
+    appId: '1:233126890615:web:63b1e912ab58a22f904a72',
+    measurementId: 'G-JS8NKWGLN2',
+  },
 };
